7a20a4fb369748d467902874f308bdb0850e9a87
Rahul Muttineni <rahulmutt@gmail.com>
2026-03-21 16:39:01 +0100

    Update tmux.lua

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -1,6 +1,11 @@
 return {
   {
     "christoomey/vim-tmux-navigator",
-    lazy = false,
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+    },
   },
 }
