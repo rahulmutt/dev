be0fb85ebcd7965b83193695b0a921f9d00905dc
Rahul Muttineni <rahulmutt@gmail.com>
2026-03-28 18:15:15 +0100

    Update post-create.sh

--- base/post-create.sh
+++ base/post-create.sh
@@ -29,6 +29,20 @@ if [ "${INSTALL_NGROK:-}" = "true" ]; then
   rm -rf /var/lib/apt/lists/*
 fi
 
+# If devenv is requested, ensure Nix is installed
+if [ "${INSTALL_DEVENV:-}" = "true" ]; then
+  INSTALL_NIX="${INSTALL_NIX:-true}"
+fi
+
+if [ "${INSTALL_NIX:-}" = "true" ]; then
+  mkdir -p /nix
+  chmod 0755 /nix
+  mkdir -p /etc/nix
+  curl -fsSL https://nixos.org/nix/install | bash -s -- --no-daemon
+  . /root/.nix-profile/etc/profile.d/nix.sh
+  echo '. /root/.nix-profile/etc/profile.d/nix.sh' >> /root/.bashrc
+fi
+
 if [ "${INSTALL_DEVENV:-}" = "true" ]; then
   . ~/.nix-profile/etc/profile.d/nix.sh
   nix profile install nixpkgs#devenv
